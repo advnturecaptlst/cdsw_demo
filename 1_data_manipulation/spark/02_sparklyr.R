a5861e2786e2f831b06a846612ef9b963bcf37a5
Ian Murray <ianmurra@justin.tv>
2019-03-26 09:51:43 -0700

    add send to local example to sparklyr

--- 1_data_manipulation/spark/02_sparklyr.R
+++ 1_data_manipulation/spark/02_sparklyr.R
@@ -26,5 +26,22 @@ flights %>%
 # In this case, the full result DataFrame is printed to the
 # screen because it's so small
 
+# can also send to local dataframe in r
+flights %>%
+  filter(dest == "LAS") %>%
+  group_by(origin) %>%
+  summarise(
+    num_departures = n(),
+    avg_dep_delay = mean(dep_delay, na.rm = TRUE)
+  ) %>%
+  arrange(avg_dep_delay) %>%
+  collect() -> result_local
+
 # End the Spark session
 spark_disconnect(spark)
+
+# check local result is still there
+result_local
+
+# can't see variables loaded into the spark session b/c it was stopped
+flights
